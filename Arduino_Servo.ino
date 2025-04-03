21fbdd060f5827ed99dc9d76da5307bf0c7493b1
K Shang <145074557+nightmimes@users.noreply.github.com>
2025-04-02 22:54:44 -0500

    Uploaded Arduino IDE code of the Servo Library
    Tested mechanics and functionality of Servo with a button. This is only the code of the Servo library, so far, to reference for our project.

---
+++ Arduino_Servo.ino
@@ -0,0 +1,54 @@
+
+/* #include <Servo.h>
+
+Servo myservo;
+const int buttonPin = 2;
+
+int buttonState = 0;
+int previousButtonState = 0;
+
+void setup() {
+  myservo.attach(9);
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+buttonState = digitalRead(buttonPin);
+Serial.print("Button State: ");
+Serial.println(buttonState);
+  
+  if (buttonState != previousButtonState) {
+    if(buttonState == HIGH){
+    myservo.write(180);
+    } else {
+    myservo.write(0);
+  }
+  }
+  previousButtonState = buttonState;
+}*/
+
+ #include <Servo.h>
+
+Servo myservo;
+const int buttonPin = 2;
+
+int buttonState = 0;
+
+void setup() {
+  myservo.attach(9);
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+buttonState = digitalRead(buttonPin);
+Serial.print("Button State: ");
+Serial.println(buttonState);
+  
+  if (buttonState == HIGH) {
+    myservo.write(180);
+  } else {
+    myservo.write(0);
+  }
+} 
